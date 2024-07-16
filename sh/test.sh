a76a47f99621cd4b6d3979b7bc5bf39560693de3
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2024-07-16 04:26:48 +0000

    The tiny set.

--- sh/test.sh
+++ sh/test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 magickCompare=''
+useSsim=0
 if [ -f "$(which magick)" ]; then
 	magickCompare='magick compare'
 elif [ -f "$(which compare)" ]; then
@@ -7,6 +8,9 @@ elif [ -f "$(which compare)" ]; then
 else
 	echo "ImageMagick is not present."
 	exit 1
+fi
+if [ "$($magickCompare -list metric | grep 'SSIM')" != '' ]; then
+	useSsim=1
 fi
 if [ "$1" == "" ]; then
 	echo "No corpus provided. A list is available below."
@@ -21,51 +25,70 @@ elif [ -d "./tmp/${1}" ]; then
 	echo "id	cat	testId	size" > "../../data/${1}.size.tsv"
 	echo "id	cat	testId	psnr" > "../../data/${1}.psnr.tsv"
 	echo "id	cat	testId	ssim" > "../../data/${1}.ssim.tsv"
+	lineCount=$(wc -l "../../corpus/${1}.tsv")
+	lineNow=1
 	cat "../../corpus/${1}.tsv" | while IFS= read -r line; do
 		if [ "$(echo $line | cut -d' ' -f1)" != "id" ]; then
+			let lineNow=lineNow+1
 			id="$(echo $line | cut -d' ' -f1)"
 			category="$(echo $line | cut -d' ' -f2)"
 			#url="$(echo $line | cut -d' ' -f3)"
 			file="$(ls -1 ${id}.* | grep -E "${id}\.(png|jpg)")"
-			echo "Working on \"$file\"..."
+			echo -e "\033[1;37mNew progress\033[0m: Working on \"$file\" at $(date "+%T")... (${lineNow}/${lineCount})"
 			# WebP lossless
-			cwebp -mt -lossless -m 6 -o "test.${id}.d0.webp" "$file"
+			#echo "Saving WebP lossless at $(date "+%T")..."
+			#cwebp -mt -lossless -m 6 -o "test.${id}.d0.webp" "$file" 2> /dev/null
 			# WebP 56dB
-			cwebp -mt -psnr 56 -qrange 90 99 -m 6 -o "test.${id}.p56.webp" "$file"
+			echo "Saving WebP PSNR 56dB at $(date "+%T")..."
+			cwebp -mt -psnr 56 -qrange 90 99 -m 6 -o "test.${id}.p56.webp" 2> /dev/null "$file"
 			# WebP q95
-			cwebp -mt -q 95 -m 6 -o "test.${id}.q95.webp" "$file"
+			echo "Saving WebP q95 at $(date "+%T")..."
+			cwebp -mt -q 95 -m 6 -o "test.${id}.q95.webp" "$file" 2> /dev/null
 			# JXL lossless
-			cjxl --num_threads -1 -j 0 -d 0 "$file" "test.${id}.d0.jxl"
+			echo "Saving JPEG XL lossless at $(date "+%T")..."
+			cjxl --num_threads -1 -j 0 -d 0 "$file" "test.${id}.d0.jxl" 2> /dev/null
 			# JXL d1.0 p
-			cjxl --num_threads -1 -j 0 -d 1 -p "$file" "test.${id}.d1.jxl"
+			echo "Saving JPEG XL d1.0 at $(date "+%T")..."
+			cjxl --num_threads -1 -j 0 -d 1 -p "$file" "test.${id}.d1.jxl" 2> /dev/null
 			# JXL d2.0 p
-			cjxl --num_threads -1 -j 0 -d 2 -p "$file" "test.${id}.d2.jxl"
+			echo "Saving JPEG XL d2.0 at $(date "+%T")..."
+			cjxl --num_threads -1 -j 0 -d 2 -p "$file" "test.${id}.d2.jxl" 2> /dev/null
 			# mozJPEG q95 np
-			cjpeg -baseline -quality 95 -optimize -outfile "test.${id}.q95.jpg" "$file"
+			echo "Saving mozJPEG q95 at $(date "+%T")..."
+			cjpeg -baseline -quality 95 -optimize -outfile "test.${id}.q95.jpg" "$file" 2> /dev/null
 			# mozJXL q95 np
-			cjxl -j 1 "test.${id}.q95.jpg" "test.${id}.q95.jxl"
+			echo "Saving mozJXL q95 at $(date "+%T")..."
+			cjxl -j 1 "test.${id}.q95.jpg" "test.${id}.q95.jxl" 2> /dev/null
 			# mozJPEG q95 p
-			cjpeg -progressive -quality 95 -optimize -outfile "test.${id}.q95p.jpg" "$file"
+			echo "Saving mozJPEG q95 progressive at $(date "+%T")..."
+			cjpeg -progressive -quality 95 -optimize -outfile "test.${id}.q95p.jpg" "$file" 2> /dev/null
 			# mozJXL q95 p
-			cjxl -j 1 "test.${id}.q95p.jpg" "test.${id}.q95p.jxl"
+			echo "Saving mozJXL q95 progressive at $(date "+%T")..."
+			cjxl -j 1 "test.${id}.q95p.jpg" "test.${id}.q95p.jxl" 2> /dev/null
 			# AVIF lossless
-			vips copy "${file}" "test.${id}.d0.avif[compression=av1,lossless=true,effort=4]"
+			#echo "Saving AVIF lossless at $(date "+%T")..."
+			#vips copy "${file}" "test.${id}.d0.avif[compression=av1,lossless=true]"
 			# AVIF q95
-			vips copy "${file}" "test.${id}.q95.avif[compression=av1,lossless=false,Q=95,effort=4]"
+			#echo "Saving AVIF q95 at $(date "+%T")..."
+			#vips copy "${file}" "test.${id}.q95.avif[compression=av1,lossless=false,Q=95]"
 			# Collecting reports...
-			echo "Collecting reports..."
+			echo "Collecting reports at $(date "+%T")..."
 			echo "${id}	${category}	source	$(wc -c ${file} | cut -d' ' -f1)" >> "../../data/${1}.size.tsv"
 			ls -1 "test.${id}."* | while IFS= read -r testfile; do
 				# Size
 				export fid="${id}"
 				export tfn="${testfile}"
 				echo "echo \"\${fid}	${category}	\${tfn/test.${fid}./}	\$(wc -c ${testfile} | cut -d' ' -f1)\"" | bash >> "../../data/${1}.size.tsv"
-				export psnr="$(${magickCompare} -metric PSNR "${file}" "${testfile}" "test.${id}.diff.png")"
+				echo "Collecting PSNR metrics at $(date "+%T")..."
+				export psnr="$(${magickCompare} -metric PSNR "${file}" "${testfile}" "test.${id}.diff.png" 2>&1)"
 				echo "echo \"\${fid}	${category}	\${tfn/test.${fid}./}	${psnr}\"" | bash >> "../../data/${1}.psnr.tsv"
-				export ssim="$(${magickCompare} -metric SSIM "${file}" "${testfile}" "test.${id}.diff.png")"
-				echo "echo \"\${fid}	${category}	\${tfn/test.${fid}./}	${ssim}\"" | bash >> "../../data/${1}.ssim.tsv"
+				if [ "$useSsim" != '0' ]; then
+					echo "Collecting SSIM metrics at $(date "+%T")..."
+					export ssim="$(${magickCompare} -metric SSIM "${file}" "${testfile}" "test.${id}.diff.png") 2>&1"
+					echo "echo \"\${fid}	${category}	\${tfn/test.${fid}./}	${ssim}\"" | bash >> "../../data/${1}.ssim.tsv"
+				fi
 			done
-			echo "Cleaning up for the next round..."
+			echo "Cleaning up for the next round $(date "+%T")..."
 			rm "test.${id}."*
 		fi
 	done
