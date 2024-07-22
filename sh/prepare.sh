10834e670aaa8f974c84e1e8155a163b575d8185
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2024-07-22 05:35:32 +0000

    Removed the older SSIM util.

--- sh/prepare.sh
+++ sh/prepare.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Setup romigrou/ssim
-echo "Building the SSIM metric CLI util..."
-git clone https://github.com/romigrou/ssim.git
-cd ssim
-make rmgr-ssim-cli
+#echo "Building the SSIM metric CLI util..."
+#git clone https://github.com/romigrou/ssim.git
+#cd ssim
+#make rmgr-ssim-cli
 exit
